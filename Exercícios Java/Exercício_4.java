73772eebcd79ba6ccaa7844d3f05c3b4f91ca497
samuca becker <samuel.becker@rede.ulbra.br>
2020-08-24 14:23:20 -0300

    Exercícios na linguagem java

---
+++ Exercícios Java/Exercício_4.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Exercício_4 {
+    public static void main(String[] args){
+        int idade, contribuição, resultado;
+        char sexo;
+
+        Scanner input = new Scanner(System.in);
+        System.out.println("Digite o seu sexo: \n M para masculino | F para feminino.");
+        
+    }
+}
